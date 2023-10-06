5e094f21b2ec4bd8b5bf599a149d621ca3afc168
Alice <alice.hsiao0811@gmail.com>
2023-10-06 18:58:05 +0800

    fix: home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import ProductCard from './components/ProductCard/ProductCard'
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
-      <Link href="/users" className='text-sky-600'>Users</Link><br />
-      <Link href="/search" className='text-sky-600'>Search</Link><br />
-      <Link href="/users2" className='text-sky-600'>Users2</Link>
-      <ProductCard />
+      <h1>User App</h1>
+      <Link href="/users" className='text-sky-600'>User List</Link><br />
+      <Link href="/search" className='text-sky-600'>Search for User</Link><br />
+      {/* <Link href="/users2" className='text-sky-600'>Users2</Link> */}
+      <ProductCard /><p>Open the console!</p>
     </main>
   )
 }
